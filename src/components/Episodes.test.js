cd041e9d41e8730febdfcfafcf8f5b8232898a58
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-22 09:44:59 -0500

    I got my episodes test to work!

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,15 +1,76 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText } from "@testing-library/react";
 import Episodes from "./Episodes";
 
+const showData = {
+  id: 2993,
+  url: "http://www.tvmaze.com/shows/2993/stranger-things",
+  name: "Stranger Things",
+  type: "Scripted",
+  language: "English",
+  image: {medium: "http://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg", 
+  original: "http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg"},
+  summary: "<p>A love letter to the '80s classics that captivated a generation, <b>Stranger Things</b> is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.</p>",
+  updated: 1586341552,
+  _embedded: {
+  episodes: [
+    {
+      id: 553946,
+      url: "http://www.tvmaze.com/episodes/553946/,stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+      name: "Chapter One: The Vanishing of Will Byers",
+      season: 1,
+      number: 1,
+      airdate: "2016-07-15",
+      airtime: "",
+      airstamp: "2016-07-15T12:00:00+00:00",
+      runtime: 60,
+      image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg", original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"},
+      summary: "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
+    },
+
+    {
+      id: 578663,
+      url: "http://www.tvmaze.com/episodes/578663/stranger-things-1x02-chapter-two-the-weirdo-on-maple-street",
+      name: "Chapter Two: The Weirdo on Maple Street",
+      season: 1,
+      number: 2,
+      airdate: "2016-07-15",
+      airtime: "",
+      airstamp: "2016-07-15T12:00:00+00:00",
+      runtime: 60,
+      image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/72/181376.jpg", original: "http://static.tvmaze.com/uploads/images/original_untouched/72/181376.jpg"},
+      summary: "<p>While the search for the missing Will continues, Joyce tells Jim about a call she apparently received from her son. Meanwhile, Jane warns Mike that there are bad people after her, and he realizes that she knows what happened to Will.</p>"
+    },
+  
+ 
+  {
+  id: 578664,
+  url: "http://www.tvmaze.com/episodes/578664/stranger-things-1x03-chapter-three-holly-jolly",
+  name: "Chapter Three: Holly, Jolly",
+  season: 1,
+  number: 3,
+  airdate: "2016-07-15",
+  airtime: "",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  runtime: 60,
+  image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168920.jpg", original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168920.jpg"},
+  summary: "<p>While Nancy looks for a missing Barbara and realizes that Jonathan may have been the last person to see her, Mike and his friends go out with Jane to find the missing Will. Meanwhile, Jim tracks Will to the lab.</p>"
+  },
+  ]
+  }
+}
 
 test ("Episodes renders without errors", () => {
     render(<Episodes episodes={[]}/>)
 });
 
-// test("Render list of episodes after API call", () => {
-//     const { rerender, queryAllByTestId } = render(<Episodes episodes={[]}/>);
-//     expect(queryAllByTestId(/episodeId/i)).toHaveLength(1);
-//     rerender(<Episodes episodes={show}/>)
-//     expect(queryAllByTestId(/episodeId/i)).toHaveLength(2)
-// })
+test("Render list of episodes after API call", () => {
+    const { rerender, queryAllByTestId, debug, getByText } = render(<Episodes episodes={[]}/>);
+
+    expect(queryAllByTestId(/episodeId/i)).toHaveLength(1);
+    
+    rerender(<Episodes episodes={showData._embedded.episodes}/>)
+    
+    expect(queryAllByTestId(/episodeId/i)).toHaveLength(1)
+    debug()
+})
